dd9c349dfd69a5c62034f24dc7dedeccc8f200b6
Jared Smith <jared.smith.jrod@gmail.com>
2022-09-06 22:59:53 +0300

    Added a Math module for general math ops
    Will change the old refs related once working with the changes (back using) the Vertex obj and matrix stuff

---
+++ src/shale/math.cr
@@ -0,0 +1,26 @@
+module Shale::Math
+  def self.parallelogram_area(*vertices : Vertex) : Float32
+    # condition was for when it was thought to be for a triangle when the
+    # following logic is the area for a parallelogram
+    #
+    # case vertices.size
+    # when .> 3
+    #   raise "Too many vertices: (#{vertices.size})"
+    # when .< 3
+    #   raise "Too few vertices: (#{vertices.size})"
+    # end
+
+    # FIXME incase this function is actually used for a 4 point shape and is
+    # included in the args
+    a, b, c = vertices
+
+    x1, y1 = b.x - a.x, b.y - a.y
+    x2, y2 = c.x - a.x, c.y - a.y
+
+    x1 * y2 - x2 * y1
+  end
+
+  def self.tirangle_area(*vertices : Vertex) : Float32
+    0.5 * parallelogram_area(*vertices)
+  end
+end
